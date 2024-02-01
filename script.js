b0260b4de509d87e3dad3534f90d5f3d60645d44
geetgovind <geetg.2902@gmail.com>
2024-02-01 16:09:10 +0530

    Added colors for dark mode

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ function toggleTheme() {
   const htmlElement = document.querySelector("html");
   htmlElement.dataset.bsTheme =
     htmlElement.dataset.bsTheme === "light" ? "dark" : "light";
+
+    var body = document.body;
+    body.style.backgroundColor = (body.style.backgroundColor === 'lightslategrey') ? 'lightgrey' :'lightslategrey' ;
+  
 }
